7142986ab8f2ab753b20d8ffc54b99aa7895e742
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-11-04 15:03:17 +0100

    user authenticated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import path from "path";
+import mongoose from 'mongoose';
+import morgan from "morgan";
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
+import auth from './routes/auth'
+
+dotenv.config()
 const app = express();
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
+app.use(morgan('dev'));
+app.use(bodyParser.json())
+
+app.use('/api/auth', auth)
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
